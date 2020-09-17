ad81a10a19ae0da9c27fd118545759607903be94
jjcaro98 <jjcaro98@hotmail.com>
2020-09-16 21:16:18 -0500

    backend v3

--- Backend/controllers/CitaController.js
+++ Backend/controllers/CitaController.js
@@ -1,17 +1,62 @@
+require('../models/Cita')
+require('../models/Paciente')
+
 const mongoose = require('mongoose')
-require('../models/Cita');
-const Cita = mongoose.model('Cita')
 const utils = require('../handlers/utils')
+const Cita = mongoose.model('Cita')
+const Paciente = mongoose.model('Paciente')
 
 exports.crearCita = (req, res) => {
+    const cedula = req.body.cedula
+    const fechaP = req.body.fecha
+    const horaP = req.body.hora
     const cita = new Cita(req.body)
-    cita.save((err, cita) => {
-        utils.show(res, err, cita)
+
+    Paciente.findById(cedula).exec((err, paciente) => {
+        if (paciente == undefined || paciente == null) {
+            res.status(201).send({
+                message: 'No se ha encontrado al paciente, no está registrado'
+            })
+        } else {
+            Cita.find({ estado: 1, hora: horaP, fecha: fechaP }).exec((e, citas) => {
+                if (err) {
+                    return res.status(500).send({ message: 'Ocurrio un error en el server', error: e })
+                }
+                if (citas.length > 0) {
+                    res.status(200).send({ message: 'No hay cita disponible a esa hora' })
+                } else {
+                    cita.save((err, cita) => {
+                        res.status(200).send({message: 'Cita creada exitosamente'})
+                        //utils.show(res, err, cita)
+                    })
+                }
+            })
+        }
     })
 }
 
 exports.traerCitas = (req, res) => {
-    Cita.find({}).exec(function (err, cita) {
+    let hora = new Date().getHours()
+    let horaLocal = new Date().toLocaleTimeString()
+    console.log(hora)
+    console.log(horaLocal)
+    Cita.find({estado: 1}).exec(function (err, cita) {
         utils.show(res, err, cita)
     })
 }
+
+// //BUSCANDO DISPONIBILIDAD
+// exports.test = (req, res) => {
+//     let fechaP = req.params.fecha
+//     let horaP = req.params.hora
+//     Cita.find({ estado: 1, hora: horaP, fecha: fechaP }).exec((err, citas) => {
+//         if (err) {
+//             return res.status(500).send({ message: 'Ocurrio un error en el server', error: err })
+//         }
+//         if (citas.length > 0) {
+//             res.status(200).send(true)
+//         } else {
+//             res.status(200).send(false)
+//         }
+//     })
+// }
--- Backend/models/Cita.js
+++ Backend/models/Cita.js
@@ -1,14 +1,14 @@
 const mongoose = require('mongoose')
 const Schema = mongoose.Schema
 const Cita = new Schema({
-    _id: Number,
     cedula: Number,
     fecha: String,
     hora: String,
     documentos: String,
     copago: Boolean,
     valorCopago: Number,
-    entidadSalud: String
+    entidadSalud: String,
+    estado: Number
 }, {
     collection: 'cita',
     toJSON: { virtuals: true }
