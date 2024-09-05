8ea28bbb9d16a1ea05d43a8717f6930fcf7a603f
santiagopc08 <santiagoperezcastro365@gmail.com>
2024-09-05 16:57:12 -0500

    run test locally to see that it is failing as well and added docker to see it fail in docker (hopefully)

--- Backend/test/routes.test.js
+++ Backend/test/routes.test.js
@@ -14,18 +14,17 @@ it('save patient data responding with status code 200', done => {
     .set('Accept', 'application/json')
     .expect('Content-type', 'application/json; charset=utf-8')
     .expect(200, done);
-    
 })
 
 //Consultar pacientes por nombre
-/*it('respond with a JSON containing all patients with the result of "like" in their name for the given text', done => {
+it('respond with a JSON containing all patients with the result of "like" in their name for the given text', done => {
     request(app)
     .get('/api/v1/paciente/nombre/sa')
     .set('Accept', 'application/json')
     .expect('Content-type', 'application/json; charset=utf-8')
     .expect(200, done);
     
-})*/
+})
 
 //Agendar una cita clinica
 it('schedule a clinic appointment responding with status code 200', done => {
@@ -63,14 +62,14 @@ it('schedule a repair appointment responding with status code 200', done => {
 })
 
 //Obtener citas clinicas
-/*it('respond with a JSON containing all clinical appointments for a given date', done => {
+it('respond with a JSON containing all clinical appointments for a given date', done => {
     request(app)
     .get('api/v1/cita-clinica/pacientes/15-8-2024')
     .set('Accept', 'application/json')
     .expect('Content-type', 'application/json; charset=utf-8')
     .expect(200, done);
     
-})*/
+})
 
 //Obtener citas de reparaciones
 it('respond with a JSON containing all repair appointments for a given date', done => {
